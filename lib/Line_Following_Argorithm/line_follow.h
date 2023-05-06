e565d998d7fc5018c59c9b85bee6d5917817c7e5
son524lt <son524lt@gmail.com>
2023-05-06 21:44:28 +0700

    shorten logging function

--- lib/Line_Following_Argorithm/line_follow.h
+++ lib/Line_Following_Argorithm/line_follow.h
@@ -12,8 +12,9 @@ private:
     float output;
     float Err=0, preErr=0;
     bool logging = false;
+    int16_t right_base_speed, left_base_speed;
 public:
-    int16_t left_pwm, right_pwm, right_base_speed, left_base_speed;
+    int16_t left_pwm, right_pwm;
     Line_follow(int16_t left_base_pwm, int16_t right_base_pwm, float _Kp, float _Ki, float _Kd);
     void calculate_output_control(bool input1, bool input2, bool input3);
     void initLogging(unsigned baudrate);
